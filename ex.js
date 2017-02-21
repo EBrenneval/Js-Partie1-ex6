4ac346502c2714c046f6d52a8c44491dc108a70b
EloB <elodie.brenneval@gmail.com>
2017-02-21 15:18:00 +0100

    Dans un fichier séparé, créer une fonction permettant d’afficher le reste de leur division. L’appeler depuis le fichier html.

---
+++ ex.js
@@ -0,0 +1,6 @@
+function modulo(){
+var nbre1 = document.getElementById("premier_nombre").value;//déclaration de la fction
+var nbre2 = document.getElementById("deuxieme_nombre").value;//declaration de la fction
+var reste = nbre1 % nbre2;//calcul du reste de la division
+alert(reste);//resultat
+}
